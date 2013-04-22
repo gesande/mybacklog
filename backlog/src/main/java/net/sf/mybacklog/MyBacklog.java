6c15cb6db4cfb725cdfdfe1bd44bb54a671fb90b
gsande <geir.sande@luukku.com>
2013-04-22 11:59:43 +0000

    started work for 1.0.3 provided tag through AbstractBacklogging

--- backlog/src/main/java/net/sf/mybacklog/MyBacklog.java
+++ backlog/src/main/java/net/sf/mybacklog/MyBacklog.java
@@ -1,9 +1,5 @@
 package net.sf.mybacklog;
 
-import static net.sf.mybacklog.MyBacklog.BacklogTag.feature;
-import static net.sf.mybacklog.MyBacklog.BacklogTag.quality;
-import static net.sf.mybacklog.MyBacklog.BacklogTag.refactoring;
-
 public class MyBacklog extends AbstractBacklogging {
 
 	public static void main(final String[] args) {
@@ -13,18 +9,25 @@ public class MyBacklog extends AbstractBacklogging {
 	public void show() {
 		newBacklog()
 				.title("my-backlog backlog")
+
 				.done()
 				.title("DONE")
-				.tasks(done("first release, quick and dirty :)", feature),
-						done("introduced chalked-backlog", refactoring),
+				.tasks(done("first release, quick and dirty :)", feature()),
+						done("introduced chalked-backlog", refactoring()),
 						done("improve test coverage for my-backlog module",
-								quality),
-						done("introduced DefaultBacklogFactory", feature),
+								quality()),
+						done("introduced DefaultBacklogFactory", feature()),
 						done("making DefaultBacklogFactory accessible to public",
-								feature))
+								feature()),
+						done("started work for version 1.0.3", newVersion()),
+						done("provide tag through AbstractBacklogging",
+								feature()))
 
-				.inProgress().title("IN PROGRESS").noTasks().waiting()
-				.title("WAITING").noTasks().show();
+				.inProgress().title("IN PROGRESS").noTasks().
+
+				waiting().title("WAITING")
+				.tasks(waiting("start work for version 1.0.4", newVersion()))
+				.show();
 	}
 
 	@Override
@@ -33,7 +36,19 @@ public class MyBacklog extends AbstractBacklogging {
 				.newBacklog();
 	}
 
-	public enum BacklogTag implements Tag {
-		refactoring, quality, feature
+	private Tag newVersion() {
+		return tag("new-version");
+	}
+
+	private Tag feature() {
+		return tag("feature");
+	}
+
+	private Tag refactoring() {
+		return tag("refactoring");
+	}
+
+	private Tag quality() {
+		return tag("quality");
 	}
 }
--- my-backlog/src/main/java/net/sf/mybacklog/AbstractBacklogging.java
+++ my-backlog/src/main/java/net/sf/mybacklog/AbstractBacklogging.java
@@ -1,28 +1,39 @@
 package net.sf.mybacklog;
 
 public abstract class AbstractBacklogging {
-    private TaskFactory taskFactory;
+	private TaskFactory taskFactory;
 
-    public AbstractBacklogging() {
-        this.taskFactory = new TaskFactory();
-    }
+	public AbstractBacklogging() {
+		this.taskFactory = new TaskFactory();
+	}
 
-    protected final InProgress inProgress(final String title, final Tag tag) {
-        return taskFactory().inProgress(title, tag);
-    }
+	protected final InProgress inProgress(final String title, final Tag tag) {
+		return taskFactory().inProgress(title, tag);
+	}
 
-    protected final Waiting waiting(final String title, final Tag tag) {
-        return taskFactory().waiting(title, tag);
-    }
+	protected final Waiting waiting(final String title, final Tag tag) {
+		return taskFactory().waiting(title, tag);
+	}
 
-    protected final Done done(final String title, final Tag tag) {
-        return taskFactory().done(title, tag);
-    }
+	protected final Done done(final String title, final Tag tag) {
+		return taskFactory().done(title, tag);
+	}
 
-    protected abstract Backlog newBacklog();
+	@SuppressWarnings("static-method")
+	protected final Tag tag(final String name) {
+		return new Tag() {
 
-    private TaskFactory taskFactory() {
-        return this.taskFactory;
-    }
+			@Override
+			public String name() {
+				return name;
+			}
+		};
+	}
+
+	protected abstract Backlog newBacklog();
+
+	private TaskFactory taskFactory() {
+		return this.taskFactory;
+	}
 
 }
